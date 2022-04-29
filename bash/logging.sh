057c605d306814505887859f256711a1e925ea58
Sean Beagle <30675442+SeanBeagle@users.noreply.github.com>
2022-04-29 11:12:41 -0400

    Update logging.sh

--- bash/logging.sh
+++ bash/logging.sh
@@ -1,11 +1,14 @@
+# Returns a timestamp in ISO 8601 Format
 timestamp() {
   echo "$(date +'%Y-%m-%dT%H:%M:%S%z')"
 }
 
+# Logs a message to STDERR
 error() {
   echo "$(timestamp): ERROR : $*" >&2
 }
 
+# Logs a message to STDOUT
 info() {
   echo "$(timestamp): INFO  : $*"
 }
